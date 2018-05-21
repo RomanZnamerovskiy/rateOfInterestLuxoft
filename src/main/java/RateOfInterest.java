2a52033e203e941cc8679ec78cac66b6c88e5fd5
rznamerovskyi <r.znamerovskiy@gmail.com>
2018-05-21 17:57:47 +0300

    Use BigDecimal instead of double

--- src/main/java/RateOfInterest.java
+++ src/main/java/RateOfInterest.java
@@ -1,11 +1,13 @@
+import java.math.BigDecimal;
+
 public class RateOfInterest {
 
-    int calculateRateOfInterest (double balance) {
-        if (balance > 0 && balance < 100) {
+    int calculateRateOfInterest (BigDecimal balance) {
+        if ( (balance.compareTo(new BigDecimal("0")) == 1) && (balance.compareTo(new BigDecimal("100")) == -1)  ) {
             return 3;
-        } else if (balance >= 100 && balance < 1000) {
+        } else if ( (balance.compareTo(new BigDecimal("100")) >= 0) && (balance.compareTo(new BigDecimal("1000")) == -1)  ) {
             return 5;
-        } else if (balance >= 1000) {
+        } else if (balance.compareTo(new BigDecimal("1000")) >= 0) {
             return 7;
         } else {
             return 0; // in case if the balance is incorrect ( zero or negative values)
--- src/test/java/TestCalculateRateOfInterest.java
+++ src/test/java/TestCalculateRateOfInterest.java
@@ -1,52 +1,68 @@
 import org.junit.*;
+
+import java.math.BigDecimal;
+
 import static junit.framework.TestCase.assertEquals;
 
 /* For manual and automation testing I will use the boundary values
-I will put next values for testing: 0.01, 99.99, 100, 999.99, 1000, 20000
-and the values for negative testing: -1, 0
+I will put next values for testing: 0.00001, 99.99999, 100, 999.99999, 1000, Double.Max_Value
+and the values for negative testing: 0, -0.00001, -Double.MAX_VALUE, "ab"
+
+During manual testing instead of Double.Max_Value I will put and verify big double value.
+
  */
 public class TestCalculateRateOfInterest {
 
     RateOfInterest rate = new RateOfInterest();
 
     @Test
-    public void testFirstRange_minValue() {
-        assertEquals("The rate of interest should be 3%",3, rate.calculateRateOfInterest(0.01));
+    public void testFirstRange_MinValue() {
+        assertEquals("The rate of interest should be 3%",3, rate.calculateRateOfInterest(new BigDecimal("0.00001")));
     }
 
     @Test
-    public void testFirstRange_maxValue() {
-        assertEquals("The rate of interest should be 3%",3, rate.calculateRateOfInterest(99.99));
+    public void testFirstRange_MaxValue() {
+        assertEquals("The rate of interest should be 3%",3, rate.calculateRateOfInterest(new BigDecimal("99.99999")));
     }
 
     @Test
-    public void testSecondRange_minValue() {
-        assertEquals("The rate of interest should be 5%",5, rate.calculateRateOfInterest(100));
+    public void testSecondRange_MinValue() {
+        assertEquals("The rate of interest should be 5%",5, rate.calculateRateOfInterest(new BigDecimal("100")));
     }
 
     @Test
-    public void testSecondRange_maxValue() {
-        assertEquals("The rate of interest should be 5%",5, rate.calculateRateOfInterest(999.99));
+    public void testSecondRange_MaxValue() {
+        assertEquals("The rate of interest should be 5%",5, rate.calculateRateOfInterest(new BigDecimal("999.99999")));
     }
 
     @Test
-    public void testThirdRange_minValue() {
-        assertEquals("The rate of interest should be 7%",7, rate.calculateRateOfInterest(1000));
+    public void testThirdRange_MinValue() {
+        assertEquals("The rate of interest should be 7%",7, rate.calculateRateOfInterest(new BigDecimal("1000")));
     }
 
     @Test
-    public void testThirdRange_anyBigValue() {
-        assertEquals("The rate of interest should be 7%",7, rate.calculateRateOfInterest(20000));
+    public void testThirdRange_DoubleMaxValue() {
+        assertEquals("The rate of interest should be 7%",7, rate.calculateRateOfInterest(new BigDecimal(Double.MAX_VALUE)));
     }
 
     @Test
-    public void testThirdRange_zeroValue() {
-        assertEquals("The rate of interest should be 0",0, rate.calculateRateOfInterest(0));
+    public void testNegativeValue_ZeroValue() {
+        assertEquals("The rate of interest should be 0",0, rate.calculateRateOfInterest(new BigDecimal("0")));
     }
 
     @Test
-    public void testThirdRange_negativeValue() {
-        assertEquals("The rate of interest should be 0",0, rate.calculateRateOfInterest(-1));
+    public void testNegativeValue_NegativeValue() {
+        assertEquals("The rate of interest should be 0",0, rate.calculateRateOfInterest(new BigDecimal("-0.00001")));
+    }
+
+    @Test
+    public void testNegativeValue_DoubleMinVAlue() {
+        assertEquals("The rate of interest should be 0",0, rate.calculateRateOfInterest(new BigDecimal(-Double.MAX_VALUE)));
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void testNegative_StringValue() {
+        rate.calculateRateOfInterest(new BigDecimal("aa"));
     }
 
 }
